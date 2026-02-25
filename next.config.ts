5fb53a570bc7bd499fc69c9d6d654006fbdf7975
hrushi <shadowmonarch666999@gmail.com>
2026-02-25 22:21:44 +0530

    Optimize for Vercel deployment

--- next.config.ts
+++ next.config.ts
@@ -17,20 +17,17 @@ const nextConfig: NextConfig = {
       },
     ],
   },
-  outputFileTracingRoot: path.resolve(__dirname, '../../'),
   typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
     ignoreBuildErrors: true,
   },
   eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
     ignoreDuringBuilds: true,
   },
-  turbopack: {
-    rules: {
-      "*.{jsx,tsx}": {
-        loaders: [loaderPath]
-      }
-    }
-  }
 } as NextConfig;
 
 export default nextConfig;
